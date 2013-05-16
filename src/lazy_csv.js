09e0a3040d174d48df184ca962670f24278c00d5
Damon Aw <daemonsy@gmail.com>
2013-05-16 17:28:54 +0100

    Introducing Lazy CSV, laziest way to do CSV

---
+++ src/lazy_csv.js
@@ -0,0 +1,35 @@
+(function( $ ) {
+
+  $.extend($.fn, {
+    tableToCSV: function() {
+      this.on("click", function(e) {
+        target = $(e.currentTarget)
+        table = $(target.data("table"));
+        if(table.length > 0) {
+          target.attr("href", readTable(table));
+        }
+      });
+    }
+
+  });
+
+  function readTable(table) {
+    results = [];
+    rows = $(table).find("tr");
+    for (var i = 0; i < rows.length; i++) {
+      row = [];
+      rows.eq(i).find("td,th").each(function(index, element) {
+        row.push(parseTextForCSV(element.innerText || element.textContent));
+      });
+      results.push(row.join(","));
+    }
+    return "data:text/csv;base64;charset=utf-8," + btoa(results.join("\r\n"));
+  }
+
+  function parseTextForCSV(text) {
+    // Changes text into CSV friendly format
+    return "\"" + text.replace(/"/g, '""').replace(/(\r\n|\n|\r)/gm,"") + "\""
+  }
+
+
+})( jQuery );
