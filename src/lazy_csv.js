5b6e3b09624fa1732246de0db38a5716635200bf
Damon Aw <daemonsy@gmail.com>
2013-05-16 18:15:27 +0100

    Tweaks

--- src/lazy_csv.js
+++ src/lazy_csv.js
@@ -17,18 +17,17 @@
     results = [];
     rows = $(table).find("tr");
     for (var i = 0; i < rows.length; i++) {
-      row = [];
+      csv_row = [];
       rows.eq(i).find("td,th").each(function(index, element) {
-        row.push(parseTextForCSV(element.innerText || element.textContent));
+        csv_row.push(parseTextForCSV(element.textContent || element.innerText));
       });
-      results.push(row.join(","));
+      results.push(csv_row.join(","));
     }
     return "data:text/csv;base64;charset=utf-8," + btoa(unescape(encodeURIComponent(results.join("\r\n"))));
   }
 
   function parseTextForCSV(text) {
-    // Changes text into CSV friendly format
-    return "\"" + text.replace(/"/g, '""').replace(/(\r\n|\n|\r)/gm,"") + "\""
+    return "\"" + text.trim().replace(/"/g, '""').replace(/(\r\n|\n|\r)/gm,"") + "\""
   }
 
 
